c9642e3bf00cc78b8ddb0ec658d874dfb89e7139
Miriam Perez <mperez1227@fullsail.edu>
2012-01-25 23:05:43 -0500

    Started phone number string.

--- Perez_Miriam_assignment4.js
+++ Perez_Miriam_assignment4.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+/*
+Miriam Perez
+January 25, 2012
+Deliverable 4
+"Library"
+*/
+
+//alert("JavaScript works!");
+
+//String
+
+//Does a string follow a 123-456-7890 pattern like a phone number?
+
+var phoneNumber = "123-456-7890";
+
+var end = phoneNumber.indexOf("-");
+
+console.log(phoneNumber.substring (0,end));
